a2378b6b4adc0bc70e7309db11e92dbec928fa2a
Tegar Lucky'Q Oakley <55655976+tegaroakley@users.noreply.github.com>
2019-10-09 06:08:38 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118066/latihan27/hurufbesarkecil/PBO210118066Latihan27HurufBesarKecil.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118066.latihan27.hurufbesarkecil;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author
+ * NAMA    : Tegar Lucky'q Oakley
+ * KELAS   : IF 2
+ * NIM     : 10118066
+ */
+public class PBO210118066Latihan27HurufBesarKecil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan Kalimat : ");
+        String kalimat = scanner.next();
+        System.out.println(" ");
+        System.out.println("====Hasil Formatting====");
+        System.out.println("Huruf Besar : " + kalimat.toUpperCase());
+        System.out.println("Huruf Kecil : " + kalimat.toLowerCase());
+    }
+    
+}
